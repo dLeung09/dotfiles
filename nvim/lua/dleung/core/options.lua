a55d718874c87d73b8e931a319937472b2015821
David Leung <davidaleung+github@gmail.com>
2024-02-28 07:01:09 -0500

    fix(nvim): removed submodule and re-added files
    This fixes an issue where changes to NVIM weren't being picked up by
    git.
    
    It seems like the issue was that the nvim/ directory was previously
    considered a submodule (since it had a .git/ directory in it) so the
    outer 'dotfiles' repo wasn't picking up any changes.

---
+++ nvim/lua/dleung/core/options.lua
@@ -0,0 +1,46 @@
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping at 132 characters (width of NVIM buffer with file explorer open, with about 4 characters gap)
+opt.wrap = true
+opt.linebreak = true
+opt.textwidth = 132
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
+
+-- Use persistent history
+opt.undofile = true
+
+-- For Obsidian UI Support
+opt.conceallevel = 2
